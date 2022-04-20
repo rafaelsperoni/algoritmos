7fb29e2dd347d61067101927ab4bee2061398e02
rafaelsperoni <rafaelsperoni@gmail.com>
2022-04-19 21:33:59 -0300

    envio inicial

---
+++ ex1.py
@@ -0,0 +1,15 @@
+# Faça um algoritmo que 
+# leia três números, 
+# calcule a soma deles e 
+# escreva o resultado.
+
+#entradas
+n1 = int(input("Informe primeiro número: "))
+n2 = int(input("Informe segundo número: "))
+n3 = int(input("Informe terceiro número: "))
+
+#processamento
+soma = n1+n2+n3
+
+#saida
+print("A soma é: ", soma)
---
+++ ex2.py
@@ -0,0 +1,7 @@
+#Faça um algoritmo que calcule a área de um terreno. Os terrenos são retangulares, e deverão ser informadas as medidas de largura e comprimento do terreno.
+larg = float(input("Informe a largura, em metros: "))
+comp = float(input("Informe o comprimento, em metros: "))
+
+area = larg * comp
+
+print("A área do terreno: ", area, " m2.")
